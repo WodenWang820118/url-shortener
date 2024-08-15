da260a79351a3b886b6d75805239226e1ec4b72f
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-08-15 10:55:33 +0800

    refactor: use VM 1. Use VirtualBox, Ubuntu VM, and Docker to run Kafka. 2. Remove manual Kafka and Zookeeper server activation.

--- apps/kafka-manager/src/main.ts
+++ apps/kafka-manager/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { KafkaManagerModule } from './kafka-manager.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(KafkaManagerModule);
--- apps/kafka-manager/src/zookeeper-server/zookeeper-server.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ZookeeperServerService } from './zookeeper-server.service';
-
-@Module({
-  imports: [],
-  providers: [ZookeeperServerService],
-  exports: [ZookeeperServerService],
-})
-export class ZookeeperServerModule {}
